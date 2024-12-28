50538490e47816ae1f58171fd271a258918ac0c3
Alper <alperkl84@gmail.com>
2024-12-28 22:18:26 +0300

    2nd attempt

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+    return (
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+            Le<b>a</b>rn by d<b>o</b>ing <br/><b>a</b>nd<br/> Tr<b>a</b>ck y<b>o</b>ur pr<b>o</b>gress
+        </div>
+)
+}
+export default AnimatedTitle
