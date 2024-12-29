4a48150f77d25149563732b5b26e382c7fd52365
Alper <alperkl84@gmail.com>
2024-12-29 03:28:38 +0300

    Update-2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import {FaGithub, FaTwitter} from "react-icons/fa";
+
+const links = [
+    { href: 'https://github.com/alperkl84/KeepUpEnglish', icon:<FaGithub /> },
+{ href: 'https://twitter.com/alperkl84', icon:<FaTwitter /> },
+]
+
+const Footer = () => {
+    return (
+        <footer className="w-screen bg-violet-300 py-4 text-black">
+            <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+                <p className="text-center text-sm md:text-left"> &copy;Alpikpikpik 2024. All rights repurposed</p>
+                <div className="flex justify-center gap-4 md:justify-start">
+                    {links.map((link) => (
+                        <a key={link} href={link.href} target="_blank" rel="noopener noreferrer" className="text-black transition-colors duration-500 ease-in-out hover:text-white">
+                            {link.icon}
+                        </a>
+                    ))}
+                </div>
+
+                <a href="#privacy-policy" className="text-center text-sm md:text-right hover:underline">
+                    Privacy Policy
+                </a>
+            </div>
+        </footer>
+    )
+}
+export default Footer
