399d33efa711e0ad4f0efddbed102586124f298b
Alper <alperkl84@gmail.com>
2024-12-29 22:56:43 +0300

    Update-4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,53 +3,51 @@ import {TiLocationArrow} from "react-icons/ti";
 import Button from "./Button.jsx";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
-
 import {ScrollTrigger} from 'gsap/all'
+import VideoPreview from "./VideoPreview.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
     const totalVideos = 4;
-    const nextVideoRef = useRef(null);
+    const nextVdRef = useRef(null);
 
     const handleVideoLoad = () => {
         setLoadedVideos((prev) => prev + 1)
     }
 
-    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+    useEffect(() => {
+            if(loadedVideos === totalVideos -1) {
+                setLoading(false);
+            }
+        }, [loadedVideos]
+    )
 
     const handleMiniVdClick = () => {
         setHasClicked(true);
 
-        setCurrentIndex(upcomingVideoIndex);
-    }
+        setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
+    };
 
-    useEffect(() => {
-        if(loadedVideos === totalVideos -1) {
-            setIsLoading(false);
-        }
-    }, [loadedVideos]
-    )
+
 
     useGSAP(()=> {
         if (hasClicked) {
             gsap.set('#next-video', { visibility: 'visible' });
-
             gsap.to('#next-video', {
                 transformOrigin: 'center center',
-                scale: 1.30,
+                scale: 1,
                 width: '100%',
                 height: '100%',
                 duration: 1,
                 ease: 'power1.inOut',
-                onStart: () => nextVideoRef.current.play(),
+                onStart: () => nextVdRef.current.play(),
             } )
-
             gsap.from('#current-video', {
                 transformOrigin: 'center center',
                 scale: 0,
@@ -61,7 +59,7 @@ const Hero = () => {
 
     useGSAP(() => {
         gsap.set('#video-frame', {
-            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            clipPath: 'polygon(14% 0%, 72% 0%, 88% 90%, 0% 95%)',
             borderRadius: '0 0 40% 10%'
         })
 
@@ -83,7 +81,7 @@ const Hero = () => {
     return (
         <div className="relative h-dvh w-screen overflow-x-hidden">
 
-            {isLoading && (
+            {loading && (
                 <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
                     <div className="three-body">
                         <div className="three-body__dot"/>
@@ -96,11 +94,13 @@ const Hero = () => {
                 <div>
                     <div
                         className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+                        <VideoPreview>
+
                         <div onClick={handleMiniVdClick}
                              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                             <video
-                                ref={nextVideoRef}
-                                src={getVideoSrc(upcomingVideoIndex)}
+                                ref={nextVdRef}
+                                src={getVideoSrc((currentIndex % totalVideos) + 1)}
                                 loop
                                 muted
                                 id="current-video"
@@ -108,15 +108,16 @@ const Hero = () => {
                                 onLoadedData={handleVideoLoad}
                             />
                         </div>
+                        </VideoPreview>
                     </div>
 
                     <video
-                        ref={nextVideoRef}
+                        ref={nextVdRef}
                         src={getVideoSrc(currentIndex)}
                         loop
                         muted
                         id="next-video"
-                        className="absolute-center invisible absolute z-20 size-64 obeject-cover object-center"
+                        className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
                         onLoadedData={handleVideoLoad}
                     />
 
